a6795a100813d2e707ed81701dc2f4b25e9d994e
Francisco Flavio C Gomes <flaviocardoso@gmail.com>
2020-08-15 18:22:42 -0300

    Update firebaseDb.js

--- src/firebaseDb.js
+++ src/firebaseDb.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQB6YnDQzzLirGjeRi0CLfT9vQT1LspWo",
-    authDomain: "vuejs-firestore-djam-crud.firebaseapp.com",
-    databaseURL: "https://vuejs-firestore-djam-crud.firebaseio.com",
-    projectId: "vuejs-firestore-djam-crud",
-    storageBucket: "vuejs-firestore-djam-crud.appspot.com",
-    messagingSenderId: "916768963356",
-    appId: "1:916768963356:web:20b155391ebc3f2a58d8d9"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
